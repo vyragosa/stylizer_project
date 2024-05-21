263766798131304fb11ae3440e7da9c7b4058ce2
uumlaut <39193849+vyragosa@users.noreply.github.com>
2024-05-21 17:09:45 +0300

    Init

---
+++ stylizer_project/urls.py
@@ -0,0 +1,40 @@
+"""
+URL configuration for stylizer_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+from stylizer_app.views import StylizeImageView
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Image Stylizer API",
+      default_version='v1',
+      description="API for image stylization",
+   ),
+   public=True,
+   permission_classes=(AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('stylize/', StylizeImageView.as_view(), name='stylize_image'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
